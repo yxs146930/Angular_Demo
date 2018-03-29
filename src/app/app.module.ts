8e3723920df358640be343dfbdc45735bb14bf2f
Patrick <icodeforyou@163.com>
2018-03-28 19:57:44 -0500

    week 5 - 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-
-
+import { AboutComponent } from './about/about.component';
+import { UniversityComponent } from './university/university.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    UniversityComponent,
+    MyprofileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
