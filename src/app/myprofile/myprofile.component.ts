e544d04f2406b8aaf4dd5a959970c55bb9916d1b
Patrick <icodeforyou@163.com>
2018-03-28 21:24:26 -0500

    week5 - 2

--- src/app/myprofile/myprofile.component.ts
+++ src/app/myprofile/myprofile.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myprofile.component.css']
 })
 export class MyprofileComponent implements OnInit {
-
-  constructor() { }
-
+  name : String = "zhouzhou";
+  name2 : String ="***********";
+  mystyle : String = "demo-profile-name-blue";
+  isDisabled : Boolean  = false;
+  
+  myExample3Name = "not set yet";
+  
+  constructor() { 
+    setTimeout(() => {
+      this.mystyle = "demo-profile-name-red";
+    }, 3000);
+  
+  }
+  
   ngOnInit() {
+
+  }
+
+  updateText(event: Event) {
+      this.myExample3Name = (<HTMLInputElement>event.target).value;
   }
 
 }
